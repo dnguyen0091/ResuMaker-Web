f1f9e324752ee826b7c2ed9e7411bdfd5f6fb76a
David Nguyen <107872059+dnguyen0091@users.noreply.github.com>
2025-03-18 18:33:49 -0400

    Add files via upload
    Current Header

---
+++ resumaker_web/src/Components/Header/header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../../assets/ResumakerLogo.png';
+import '../../index.css';
+import NavBar from '../Navigation/navBar.jsx';
+import './header.css';
+export default function Header() {
+    return (
+        <div className="header">
+            <div className="header-content">
+                <div className="logo-container">
+                    <img src={logo} alt="ResuMaker Logo" className="logo" />
+                </div>
+                
+                <div className="right-content">
+                    <NavBar/>
+                </div>
+            </div>
+        </div>
+    );
+}
