263c33e75b1d65fb5b9de02278de0e8309f83dff
David Nguyen <107872059+dnguyen0091@users.noreply.github.com>
2025-03-26 12:46:40 -0400

    Add files via upload

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,27 +1,14 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 import logo from '../../assets/Resumaker.png';
 import '../../index.css';
 import NavBar from '../Navigation/navBar.jsx';
 import './header.css';
 export default function Header() {
-    const [timesClicked, setTimesClicked] = useState(0);
-    const easterEggAudio = useRef(new Audio('https://www.myinstants.com/media/sounds/roblox-death-sound_1.mp3'));
-
-    const handleClick = () => {
-        setTimesClicked(prev => {
-            const newCount = prev + 1;
-            if (newCount % 5 === 0) {
-                console.log('You have clicked the logo 5 times!');
-                easterEggAudio.current.play();
-            }
-            return newCount;
-        });
-    };
     return (
         <div className="header">
             <div className="header-content">
                 <div className="logo-container">
-                    <img src={logo} alt="ResuMaker Logo" className="logo" onClick={handleClick}/>
+                    <img src={logo} alt="ResuMaker Logo" className="logo"/>
                 </div>
                 
                 <div className="right-content">
