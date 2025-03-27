c50dfe41202e457fc9d23266f4ede36c4ffa6772
JuniperDeSanto <juniperdesanto@gmail.com>
2025-03-26 23:33:20 -0400

    Update auth.jsx
    included new endpoints. verifyForgot and updatePassword are not operable

--- src/api/auth.jsx
+++ src/api/auth.jsx
@@ -16,3 +16,23 @@ export const apiCheckExists = async (email) => {
   const response = await axios.post(`${API_URL}/auth/checkExists`, { email });
   return response.data;
 }
+
+export const apiForgotPassword = async(email) => {
+  const response = await axios.post(`${API_URL}/auth/forgotPassword`, { email });
+  return response.data;
+}
+
+export const apiVerifyEmail = async(email, verificationCode) => {
+  const response = await axios.post(`${API_URL}/auth/verifyEmail`, { email, verificationCode });
+  return response.data;
+}
+
+export const apiUpdatePassword = async(email, password) => {
+  const response = await axios.post(`${API_URL}/auth/updatePassword`, { email, password });
+  return response.data;
+}
+
+export const apiVerifyForgot = async(email, verificationCode) => {
+  const response = await axios.post(`${API_URL}/auth/verifyForgot`, { email, verificationCode });
+  return response.data;
+}
